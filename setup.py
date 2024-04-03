e43fd49d0e3adb69affe051f7e9e0d46c22d1f4a
Idris <dri.taha24@gmail.com>
2024-04-03 11:23:49 +0300

    Fixing and publishing

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='geometool',
     version='0.0.1',
@@ -11,6 +14,8 @@ setup(
     author='Idris Taha',
     author_email='dri.taha24@gmail.com',
     description='A Geometric Shape Calculator Tool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/K4speeer/Geometry_Tool',
     classifiers=[
         'Programming Language :: Python :: 3',
