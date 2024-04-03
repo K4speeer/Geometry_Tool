c55a4298389a45238da3f384ac4fb91818b9ee51
Idris <dri.taha24@gmail.com>
2024-04-03 19:13:15 +0300

    Fixing dependencies

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+with open("requirements.txt", "r") as f:
+    required_packages = f.read().splitlines()
+    
+    
 setup(
     name='geometool',
     version='0.0.7',
     packages=['geometool'],
-    install_requires=[
-        'math'
-        'abc'
-    ],
+    install_requires= required_packages,
     author='Idris Taha',
     author_email='dri.taha24@gmail.com',
     description='A Geometric Shape Calculator Tool',
